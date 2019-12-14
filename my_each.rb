1131ffdefd4586721fa41f0c3cb047ffac82cee5
Amanda Rolon <abrolon87@gmail.com>
2019-12-14 21:50:53 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -6,7 +6,6 @@ def my_each(collection) # put argument(s) here
   end# code here
 end
 
-my_each(words) do |word|
-  if words.include?("collection")
-  end
+my_each(collection) do |word|
+  puts word
 end
