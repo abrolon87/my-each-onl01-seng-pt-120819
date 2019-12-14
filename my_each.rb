a85d21775096676ca76da294416430e0b6aa0655
Amanda Rolon <abrolon87@gmail.com>
2019-12-14 21:39:52 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,5 @@
 def my_each(collection) # put argument(s) here
   counter = 0
   while counter < collection.length
-    yield collection
-    counter + 1
-  end
-  return collection# code here
+    counter = counter + 1# code here
 end
