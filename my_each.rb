4e46573f401d5fb3fa674eab36d645dbdf2bd76d
Amanda Rolon <abrolon87@gmail.com>
2019-12-14 21:46:44 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -6,7 +6,7 @@ def my_each(collection) # put argument(s) here
   end# code here
 end
 
-my_each(tas) do |ta|
-  if ta.include?("collection")
+my_each(words) do |word|
+  if words.include?("collection")
   end
 end
